4836d572a006e3ed97956f386c7b86fb78746863
Jan <janpc01@gmail.com>
2025-03-27 12:28:07 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     // apiUrl: 'http://ec2-52-73-121-14.compute-1.amazonaws.com/'  // Development API URL
-    apiUrl: 'http://localhost:4000'
+    apiUrl: 'https://tempo-ror-backend.onrender.com'
   };
